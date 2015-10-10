54342f6e5b84cb884ea2f2c18fd8f5908099dc1d
Mark Bloechl <mark@mezzolux.com>
2015-10-10 14:56:18 -0400

    working on views

--- src/scripts/views/homeView.js
+++ src/scripts/views/homeView.js
@@ -1,14 +1,21 @@
 //  views
 import Backbone from "backbone";
 import Listing from "../models/listing";
+import _ from "underscore";
+
+_.templateSettings = {
+  interpolate: /\{\{(.+?)\}\}/g
+};
 
 let HomeView = Backbone.View.extend({
 
-	model: Listing,
+	template: _.template($("#homeTemplate").text()),
 
 	render: function () {
-		console.log(this.model.id);
+		this.$el.html(this.template(this.model.attributes)
+		return this.$el;
 	}
 
 });
+
 export default HomeView;
